9c8f2e21302e4de64199d643726d52457e9506f8
SpikeBasel <spikerine@gmail.com>
2022-11-17 18:39:42 +0500

    Added Lesson List

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <LessonList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LessonList from "@/components/LessonList.vue";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+  components: {
+    LessonList,
+  },
+};
+</script>
